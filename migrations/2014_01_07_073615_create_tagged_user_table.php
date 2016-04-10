0ce0c9e311f003884aaaba476b1998ddc3caf209
danrichards <danrichardsri@gmail.com>
2016-04-10 12:51:12 -0400

    Table to track user tags.

--- migrations/2014_01_07_073615_create_tagged_user_table.php
+++ migrations/2014_01_07_073615_create_tagged_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 /**
- * Class CreateUserTaggedTable
+ * Class CreateTaggedUserTable
  *
  * Migration to create a pivot table for connecting users that have tagged
  * specific models with specific tags.
@@ -15,10 +15,8 @@ class CreateTaggedUserTable extends Migration {
 		Schema::create('tagging_tagged_user', function(Blueprint $table) {
 			$table->increments('id');
 			$table->integer('tagged_id')->unsigned()->index();
-			$table->integer('user_id');
+			$table->integer('user_id')->unsigned()->index();
 			$table->timestamps();
-			$table->index(['tagged_id'], 'tagging_tagged_id');
-			$table->index(['user_id'], 'tagging_user_id');
 			$table->index(['tagged_id', 'user_id'], 'tagging_tagged_user_ids');
 		});
 	}
